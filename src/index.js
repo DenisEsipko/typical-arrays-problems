a1cef3d4be449f2bcc72d5b56d8189c202ef280e
DenisEsipko <deni007a1@gmail.com>
2021-08-08 12:52:06 +0300

    init: final

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = 0) {
+    return array.length > 0 ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = 0) {
+    return array.length > 0 ? Math.max(...array) : 0;
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = 0) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return array.length > 0 ? array.reduce(reducer, 0) / array.length : 0;
 }
+
